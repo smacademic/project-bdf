167fed63d633302b9c9ac168b6807833554c48b3
Brian Bacon <bacon011@connect.wcsu.edu>
2019-03-18 19:52:27 -0400

    Update TextifyBot.py

--- src/TextifyBot.py
+++ src/TextifyBot.py
@@ -3,8 +3,10 @@
 # This python script identified comments that should eventually be processed by our bot
 # within a specific set of subreddits
 
+import sys
 import praw
 from praw.models import Comment
+import re
 
 KEYWORD = '!TextifyReddit' # what to look for to know when the bot is called
 SUBREDDIT = 'BDFTest' # subreddit to search for comments in (multiple subreddits
@@ -19,6 +21,19 @@ reddit = praw.Reddit(client_id='', # 14 characters
 numComments = 0 # total number of comments parsed
 numRequests = 0 # number of comments with KEYWORD
 
+# splits comment sentence into individual words then parses words 
+# to find if they end in popular image format
+def extractURL(comment):
+    commentSplit = re.split('\s', comment)
+    
+    for word in commentSplit:
+        if re.search('(^.*\.jpg\)$)', word) != None:
+            return word
+        elif re.search('(^.*\.png\)$)', word) != None:
+            return word
+        elif re.search('(^.*\.tif\)$)', word) != None:
+            return word
+
 for submission in reddit.subreddit(SUBREDDIT).hot(limit=10):
     # this code is not well-optimized for deeply nested comments
     # see https://praw.readthedocs.io/en/latest/code_overview/models/comment.html#praw.models.Comment.parent
@@ -27,8 +42,12 @@ for submission in reddit.subreddit(SUBREDDIT).hot(limit=10):
         if (comment.body.find(KEYWORD)) >= 0:
             numRequests += 1
             if isinstance(comment.parent(), Comment):
+                url = extractURL(comment.parent().body)
                 print('Comment to textify:')
                 print(comment.parent().body)
+                if url != None:
+                    print('URL found:')
+                    print(url)
             else:
                 print('Submission to textify:')
                 print(comment.parent().url)
