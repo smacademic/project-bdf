e79ec9ebf24dc9497804323b09530a18fda6858b
Andrew Figueroa <afig23@gmail.com>
2019-03-20 19:28:44 -0400

    Add image transcription from example code
    This commit add image transcription using the code that @calebeda added in the examples folder.
    
    Looks like we will need to do some pre-processing on images, but overall its working to some extent.

--- src/TextifyBot.py
+++ src/TextifyBot.py
@@ -4,32 +4,26 @@
 # within a specific set of subreddits
 
 import os
-import time # temporary for testing image saving
 import urllib.request
 import botSetup
 import praw
+import PIL
+import pytesseract
 
 
 KEYWORD = '!TextifyReddit' # what to look for to know when the bot is called
 SUBREDDIT = 'BDFTest' # subreddit to search for comments in (multiple subreddits
                       # can be specified by placing a '+' between them)
 IMAGE_DIR = 'images/' # directory to temporarily download images to
+TESSERACT_PATH = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 
-numComments = 0 # total number of comments parsed
-numRequests = 0 # number of comments with KEYWORD
-
-
-bot = botSetup.textify_login()
-
-def findTextInSubreddit(connection, sub): # pass subreddit name in to search for !TextifyReddit
+def findTextInSubreddit(connection, sub, keyword): # pass subreddit name in to search for !TextifyReddit
     for submission in connection.subreddit(sub).hot(limit=10):
         # this code is not well-optimized for deeply nested comments
         # see https://praw.readthedocs.io/en/latest/code_overview/models/comment.html#praw.models.Comment.parent
         for comment in submission.comments.list():
-            numComments += 1
-            if (comment.body.find(KEYWORD)) >= 0:
-                numRequests += 1
+            if (comment.body.find(keyword)) >= 0:
                 if isinstance(comment.parent(), praw.models.Comment):
                     urls = botSetup.extractURL(comment.parent().body)
                     print('Comment to textify:')
@@ -37,15 +31,21 @@ def findTextInSubreddit(connection, sub): # pass subreddit name in to search for
                     if urls != None:
                         print('URL(s) found:')
                         print(urls)
-                        transcribeImages(urls)
+                        print('Text transcribed:')
+                        print(transcribeImages(urls))
                 else:
                     print('Submission to textify:')
                     print(comment.parent().url)
 
 
-# download image given a URL
-imagesToDL = urls
-def transcribeImages(imagesToDL): # list of image URLs to download
+def tesseractTranscribe(imagePath):
+    image = PIL.Image.open(imagePath)
+    pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
+
+    return pytesseract.image_to_string(image)
+
+
+def transcribeImages(imagesToDL): # download and transcribe a list of image URLs
     transcribedText = []
     for imageURL in imagesToDL:
         imageURL = imageURL.rstrip('/')
@@ -55,13 +55,18 @@ def transcribeImages(imagesToDL): # list of image URLs to download
         
         if imageName != '':
             imagePath = IMAGE_DIR + imageName
-            urllib.request.urlretrieve(imageURL, imagePath)
-            time.sleep(5) # TEMP: Allows time for inspecting image
-            # TODO: Process image
-            os.remove(imagePath)
 
+            try:
+                urllib.request.urlretrieve(imageURL, imagePath)
+                imageText = tesseractTranscribe(imagePath)
+                transcribedText.append(imageText)
+                os.remove(imagePath)
+            except urllib.error.HTTPError as e:
+                print("WARNING: HTTPError when downloading " + imageURL + ":\n")
+                print(str(e) + '\n')
     return transcribedText
 
 
-print('Number of comments parsed: ', numComments)
-print('Number of TextifyCalls identified: ', numRequests)
+# Main driver code
+bot = botSetup.textify_login()
+findTextInSubreddit(bot, SUBREDDIT, KEYWORD)
